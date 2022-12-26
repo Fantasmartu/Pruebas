bc879358fce8534b2a84a0016b66844699fe6e46
Fantasmartu <121270933+Fantasmartu@users.noreply.github.com>
2022-12-26 03:28:28 +0000

    completo Ejercicio Completado

--- ejercicio1.py
+++ ejercicio1.py
@@ -17,3 +17,23 @@ if Clave == 1234:
         print(Nombre + " Usted tiene 20 días de vacaciones.")
     else:
         print(Nombre + " Usted no tiene derecho a vacaciones.")
+elif Clave == 4321:
+    if Años == 1:
+        print(Nombre + " Usted tiene 7 días de vacaciones.")
+    elif (Años >= 2 )and (Años <= 6):
+        print(Nombre + " Usted tiene 15 días de vacaciones.")
+    elif Años >= 7:
+        print(Nombre + " Usted tiene 22 días de vacaciones.")
+    else:
+        print(Nombre + " Usted no tiene derecho a vacaciones.")
+elif Clave == 4231:
+    if Años == 1:
+        print(Nombre + " Usted tiene 10 días de vacaciones.")
+    elif (Años >= 2 )and (Años <= 6):
+        print(Nombre + " Usted tiene 20 días de vacaciones.")
+    elif Años >= 7:
+        print(Nombre + " Usted tiene 30 días de vacaciones.")
+    else:
+        print(Nombre + " Usted no tiene derecho a vacaciones.")
+else:
+    print ("Clave Incorrecta")
