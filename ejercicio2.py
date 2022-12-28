0e1af02c0ca53d1887bbc92517c96b82fe560eb2
Fantasmartu <121270933+Fantasmartu@users.noreply.github.com>
2022-12-28 04:49:38 +0000

    ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,12 @@
+print("==============================================")
+print("=.:Identificador de numeros pares o impares:.=")
+print("==============================================")
+numero = int(input("Ingrese el numero a identificar."))
+
+print("==============================================")
+if numero % 2 == 0:
+    print ("El numero es par")
+    print("==============================================")
+elif numero % 2 == 1:
+    print("El numero es impar")
+    print("==============================================")
